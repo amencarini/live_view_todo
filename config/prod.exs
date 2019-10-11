bf32bb75767ee01115daf1256bdc2efd5a3d488e
Alessandro Mencarini <alessandromencarini@monzo.com>
2019-10-11 22:32:58 +0100

    Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_view_todo, LiveViewTodoWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "live-view-todo.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["//localhost", "//live-view-todo.herokuapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
--- lib/live_view_todo_web/endpoint.ex
+++ lib/live_view_todo_web/endpoint.ex
@@ -2,10 +2,10 @@ defmodule LiveViewTodoWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :live_view_todo
 
   socket "/socket", LiveViewTodoWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket
+  socket "/live", Phoenix.LiveView.Socket, websocket: [timeout: 45_000]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
